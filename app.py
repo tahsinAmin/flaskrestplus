21f9185d3c8e25f90f22178bda44f420fbeefc40
Muhammad Tahsin Amin <muhammadtahsinamin@gmail.com>
2022-01-18 19:56:30 +0600

    05:00

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
+import sqlite3
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///db.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+app.config['SECRET_KEY']=True
+db = SQLAlchemy(app)
+
+#Table
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name=db.Column(db.String)
+    email=db.Column(db.String)
+    password=db.Column(db.String)
+
+@app.route('/')
+def get():
+    return 'ok'
