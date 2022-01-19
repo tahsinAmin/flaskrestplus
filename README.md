0909240b24934f1841d0c868bd6c9a5aae1a051b
tahsinAmin <muhammadtahsinamin@gmail.com>
2022-01-19 22:24:11 +0600

    Update till 10:00

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-<!-- https://www.youtube.com/watch?v=vYubIZOyLfI -->
+<!-- https://www.youtube.com/watch?v=vYubIZOyLfI --> - 10:03
+<!-- https://github.com/itsmaheshkariya/flask-restplus/blob/master/app.py -->
+
+# Setup
+
+```
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+flask run
+```
+
+```
+http://127.0.0.1:5000/
+```
+
+python3 -m venv env
--- app.py
+++ app.py
@@ -1,29 +1,55 @@
-import sqlite3
+# import sqlite3
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-
+from flask_restplus import Api, fields, Resource
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///db.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
-app.config['SECRET_KEY']=True
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# app.config['SECRET_KEY'] = True
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+api = Api()
+api.init_app(app)
 
-#Table
+# Table
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name=db.Column(db.String)
-    email=db.Column(db.String)
-    password=db.Column(db.String)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    password = db.Column(db.String)
+
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name','email', 'password')
+        fields = ('id', 'name', 'email', 'password')
+
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
-@app.route('/')
-def get():
-    return 'ok'
+
+@api.route('/')
+class getdata(Resource):
+    def get():
+        return {"message": "Working"}
+
+
+@api.route('/post')
+class postData(Resource):
+    def get():
+        return {"message": "Working"}
+
+
+@api.route('/put/<int:id>')
+class putData(Resource):
+    def put(self, id):
+        return {"message": id}
+
+
+@api.route('/delete/<int:id>')
+class deleteData(Resource):
+    def delete(self, id):
+        return {"message": id}
