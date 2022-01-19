d11cf26219899e2ef10259092ef099bc514a0720
Muhammad Tahsin Amin <muhammadtahsinamin@gmail.com>
2022-01-19 19:21:55 +0600

    Office update .19

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import sqlite3
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///db.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 app.config['SECRET_KEY']=True
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 #Table
 class User(db.Model):
@@ -13,6 +16,13 @@ class User(db.Model):
     name=db.Column(db.String)
     email=db.Column(db.String)
     password=db.Column(db.String)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name','email', 'password')
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
 
 @app.route('/')
 def get():
